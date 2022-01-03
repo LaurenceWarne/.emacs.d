3ba7b8f3734607f325ee49d5b52a577347a806d7
lWarne <laurencewarne@gmail.com>
2022-01-03 22:57:18 +0000

    Update ox-yaow

--- lisp/package-init.el
+++ lisp/package-init.el
@@ -831,31 +831,40 @@
   :after org
   :config
   ;; Stolen from https://github.com/fniessen/org-html-themes
-  (setq rto-css '("https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css"
-                  "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css")
-        rto-js '("https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js"
-                 "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js")
-        extra-js '("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
-                   "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" )
-        ox-yaow-html-head (concat (mapconcat (lambda (url) (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" url "\"/>\n")) rto-css "")
-                                  (mapconcat (lambda (url) (concat "<script src=\"" url "\"></script>\n")) (append rto-js extra-js) ""))
-        org-publish-project-alist (cons
-                                   `("wiki"
-                                     :base-directory "~/org/"
-                                     :base-extension "org"
-                                     :publishing-directory "~/wiki/"
-                                     :html-head ,ox-yaow-html-head
-                                     :html-preamble t
-                                     :recursive t
-                                     :exlude ".*steam\.org"
-                                     :publishing-function ox-yaow-publish-to-html
-                                     :preparation-function ox-yaow-preparation-fn
-                                     :completion-function ox-yaow-completion-fn
-                                     :ox-yaow-wiki-home-file "~/org/wiki.org"
-
-                                     :ox-yaow-file-blacklist ("~/org/maths/answers.org")
-                                     :ox-yaow-depth 2)
-                                   org-publish-project-alist)))
+  (let* ((rto-css '("https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/htmlize.css"
+                    "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/css/readtheorg.css"))
+         (rto-js '("https://fniessen.github.io/org-html-themes/src/lib/js/jquery.stickytableheaders.min.js"
+                   "https://fniessen.github.io/org-html-themes/src/readtheorg_theme/js/readtheorg.js"))
+         (extra-js '("https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"
+                     "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js" ))
+         (ox-yaow-html-head (concat (mapconcat (lambda (url) (concat "<link rel=\"stylesheet\" type=\"text/css\" href=\"" url "\"/>\n")) rto-css "")
+                                    (mapconcat (lambda (url) (concat "<script src=\"" url "\"></script>\n")) (append rto-js extra-js) ""))))
+    (setq org-publish-project-alist
+          (append
+           `(("wiki-pages"
+              :base-directory "~/org/"
+              :base-extension "org"
+              :publishing-directory "~/wiki/"
+              :html-head ,ox-yaow-html-head
+              :html-preamble t
+              :recursive t
+              :exclude ".*steam.*"
+              :publishing-function ox-yaow-publish-to-html
+              :preparation-function ox-yaow-preparation-fn
+              :completion-function ox-yaow-completion-fn
+              :ox-yaow-wiki-home-file "~/org/wiki.org"
+              :ox-yaow-file-blacklist ("~/org/maths/answers.org")
+              :ox-yaow-depth 2)
+             ("wiki-static"
+              :base-directory "~/org/"
+              :base-extension "css\\|js\\|png\\|jpg\\|gif\\|pdf\\|mp3\\|ogg\\|swf"
+              :publishing-directory "~/wiki/"
+              :recursive t
+              :exclude ,(rx (0+ ".") (or "steam" "ltximg") (0+ "."))
+              :publishing-function org-publish-attachment)
+             ("wiki"
+              :components ("wiki-pages" "wiki-static")))
+           org-publish-project-alist))))
 
 ;; https://github.com/yoshiki/yaml-mode
 (use-package yaml-mode)
