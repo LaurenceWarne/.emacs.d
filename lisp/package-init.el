75603ccad9c92c5a3c9446395eb39e9f936036ac
Laurence Warne <laurencewarne@gmail.com>
2022-12-27 16:10:10 +0000

    Add mysql to docker custom args

--- lisp/package-init.el
+++ lisp/package-init.el
@@ -1252,7 +1252,11 @@
    `(".*jaegertracing.*"
      ;; For the Jaeger UI open:
      ;; http://localhost:16686/
-     ("-d" "--name jaeger" "-e COLLECTOR_ZIPKIN_HOST_PORT=:9411" "-p 80:80 -p 5775:5775/udp -p 6831:6831/udp -p 6832:6832/udp -p 5778:5778 -p 16686:16686 -p 14268:14268 -p 14250:14250 -p 9411:9411" . ,docker-image-run-default-args))))
+     ("-d" "--name jaeger" "-e COLLECTOR_ZIPKIN_HOST_PORT=:9411" "-p 80:80 -p 5775:5775/udp -p 6831:6831/udp -p 6832:6832/udp -p 5778:5778 -p 16686:16686 -p 14268:14268 -p 14250:14250 -p 9411:9411" . ,docker-image-run-default-args)))
+  (add-to-list
+   'docker-image-run-custom-args
+   `(".*mysql.*"  ; https://hub.docker.com/_/mysql/
+     ("--name some-mysql" "-e MYSQL_ROOT_PASSWORD=root" . ,docker-image-run-default-args))))
 
 ;; https://github.com/jcs-elpa/goto-line-preview
 (use-package goto-line-preview
