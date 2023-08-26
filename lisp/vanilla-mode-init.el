588cbb26086b26716d731318c5267166c748b9a4
Laurence Warne <laurencewarne@gmail.com>
2023-08-26 21:53:16 +0100

    Rebind C-x C-f to ffap

--- lisp/vanilla-mode-init.el
+++ lisp/vanilla-mode-init.el
@@ -231,3 +231,9 @@
   (defun eclipse-indent-setup ()
     (c-set-offset 'arglist-intro '+)
     (c-set-offset 'arglist-close 0)))
+
+(use-package ffap
+  :ensure nil
+  :demand t
+  :config
+  (ffap-bindings))
