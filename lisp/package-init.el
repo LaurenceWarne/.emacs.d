4b55c614a9563ebb826ac995d6715864c735d945
Laurence Warne <laurencewarne@gmail.com>
2025-06-13 18:23:19 +0100

    Add more chosen rom-party words

--- lisp/package-init.el
+++ lisp/package-init.el
@@ -2267,7 +2267,7 @@ directory is part of a projectile project."
 (use-package rom-party
   :commands (rom-party rom-party-infinite rom-party-choose-configuration)
   :config
-  (setq rom-party-chosen-words '("rne" "garner" "tco" "outcome" "hra" "thrash" "oic" "voice" "isu" "visual" "rbu" "overburden" "mv" "triumvirate" "uiv" "quiver" "rpa" "vorpal" "rdr" "airdrop" "umf" "humf" "ak" "yak" "elc" "belch" "llh" "hellhole" "gho" "ghost" "ehy" "dehydrate" "rtn" "partner" "olc" "volcano" "oei" "onomatopoeia" "amu" "gamut" "shm" "shmuck" "wc" "showcase" "ewo" "rework" "efu" "useful" "myo" "myopic" "ldo" "seldom" "vem" "movement" "rw" "narwhal" "df" "dreadful" "eun" "eunuch" "yne" "slyness" "efr" "refresh" "ptr" "sceptre" "apl" "maple" "hh" "shh" "skr" "skry" "bp" "subpar" "sg" "misgive")))
+  (setq rom-party-chosen-words '("rne" "garner" "tco" "outcome" "hra" "thrash" "oic" "voice" "isu" "visual" "rbu" "overburden" "mv" "triumvirate" "uiv" "quiver" "rpa" "vorpal" "rdr" "airdrop" "umf" "humf" "ak" "yak" "elc" "belch" "llh" "hellhole" "gho" "ghost" "ehy" "dehydrate" "rtn" "partner" "olc" "volcano" "oei" "onomatopoeia" "amu" "gamut" "shm" "shmuck" "wc" "showcase" "ewo" "rework" "efu" "useful" "myo" "myopic" "ldo" "seldom" "vem" "movement" "rw" "narwhal" "df" "dreadful" "eun" "eunuch" "yne" "slyness" "efr" "refresh" "ptr" "sceptre" "apl" "maple" "hh" "shh" "skr" "skry" "bp" "subpar" "sg" "misgive" "ioc" "idiocy" "iy" "hiya" "ysa" "naysayer" "uld" "would" "inh" "sinh" "eor" "meteor" "aln" "walnut" "ssw" "password" "seu" "museum" "reu" "vitreum" "gmo" "sigmoid")))
 
 ;; https://github.com/colonelpanic8/multi-line
 (use-package multi-line
